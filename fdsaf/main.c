0f3d21f16481022c39ecfb4a07c761c4b6a61d82
ivanbarreira <ivanchooh@gmail.com>
2019-03-20 10:44:12 -0300

    for, while y do while

---
+++ fdsaf/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//CON WHILE
+/*int main()
+{
+    int i;
+    i=0;
+
+    while(i<10)
+    {
+        printf("%d\n",(i+1));
+        i++;
+    }
+    return 0;
+}*/
+
+
+//CON DO WHILE
+/*int main()
+{
+    int i;
+    i=0;
+
+    do
+    {
+        i++;
+        printf("%d\n",i);
+    }while(i<10);
+
+    return 0;
+}*/
+
+
+//CON FOR
+
+/*int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",i+1);
+    }
+
+    return 0;
+}*/
